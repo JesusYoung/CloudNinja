58340c8b3e6e031d3d1d04598ded6da50e93e854
JesusYoung <jesus_yangshijie@163.com>
2021-11-25 16:30:20 +0800

    add monitor-server prometheus config

--- monitor-server/src/main/java/cn/com/yangshj/monitor/MonitorServerApplication.java
+++ monitor-server/src/main/java/cn/com/yangshj/monitor/MonitorServerApplication.java
@@ -1,8 +1,12 @@
 package cn.com.yangshj.monitor;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -12,4 +16,10 @@ public class MonitorServerApplication {
         SpringApplication.run(MonitorServerApplication.class, args);
     }
 
+    // prometheus + grafana + springboot2监控集成配置
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+                return (registry) -> registry.config().commonTags("application", applicationName);
+    }
 }
